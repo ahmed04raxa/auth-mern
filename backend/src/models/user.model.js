849e37f86353896407e9c59188d1bc51dc2c6c0f
Ahmed Raza <ahmed04raxa@gmail.com>
2026-04-24 09:27:03 +0500

    User Model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            minLength: [3, "Name must have atleast 3 characters!"]
+        },
+        email: {
+            type: String,
+            required: true,
+            validate: [validator.isEmail, "Please provide a valid email address"]
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: [8, "Password must have atleast 8 characters!"]
+        }
+    }
+)
+
+userSchema.pre('save',
+    async function (next) {
+        if (!this.isModified("password")) return next()
+        this.password = await bcrypt.hash(this.password, 10)
+    }
+)
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+export const User = mongoose.model("User", userSchema)

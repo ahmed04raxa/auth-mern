98086457457c657c94d2552afcbbefb0db2f0998
Ahmed Raza <ahmed04raxa@gmail.com>
2026-04-25 22:00:33 +0500

    Some Bug Fixes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,44 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../context/UserContext'
-import { Navigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 const Home = () => {
-  const { isAuthenticated } = useContext(UserContext)
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(UserContext)
+  const navigate = useNavigate()
   if (!isAuthenticated) {
     return <Navigate to={"/login"} />
+  }
+  const logout = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/user/logout",
+        {},
+        { withCredentials: true }
+      )
+
+      toast.success(response.data.message)
+
+      setIsAuthenticated(false)
+      setUser(null)
+      navigate("/login")
+
+    } catch (error) {
+      console.log(error)
+
+      const message =
+        error?.response?.data?.message ||
+        error?.message ||
+        "Logout failed"
+
+      toast.error(message)
+    }
   }
   return (
     <>
       {
-        isAuthenticated ? <button className='btn'>LOGOUT</button> : ""
+        isAuthenticated ? <button className='btn' onClick={logout}>LOGOUT</button> : ""
       }
     </>
 
